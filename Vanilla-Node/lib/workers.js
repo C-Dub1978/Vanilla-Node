0902e9649f136a3d1ebf8303c35b4d25cade6c20
TransientCW <chris@myubercode.com>
2020-02-24 22:32:59 -0700

    got initial workers up and functioning, need to test

--- Vanilla-Node/lib/workers.js
+++ Vanilla-Node/lib/workers.js
@@ -1,7 +1,7 @@
 // Deps
 const config = require('../config');
 const fs = require('fs');
-const helpers = require('helpers');
+const helpers = require('./helpers');
 const http = require('http');
 const https = require('https');
 const _lib = require('./data');
@@ -13,6 +13,27 @@ const MINUTE_INTERVAL = 60000;
 
 const workers = {};
 
+/**
+ * Send an SMS message to user because of check state change
+ *
+ * @param {check} the health check object
+ */
+workers.alertUserToStateChange = check => {
+  const message = `Alert! Your check for:
+  ${check.method.toUpperCase()} ${check.protocol}://${check.url}
+  is current in a state of '${check.state}'`;
+  helpers.sendTwilioSms(check.phoneNumber, message, err => {
+    if (!err) {
+      console.log('Alerted user to check state change with SMS');
+    } else {
+      console.log('Error in SMS alert: ', err);
+    }
+  });
+};
+
+/**
+ * Init method to get all checks and continue checking with a loop
+ */
 workers.initialize = () => {
   // Execute all checks
   workers.gatherAllChecks();
@@ -58,7 +79,108 @@ workers.loop = () => {
  *
  * @param {check} the health check
  */
-workers.performCheck = check => {};
+workers.performCheck = check => {
+  // we havent sent any http request so these properties are false
+  const outcome = {
+    error: false,
+    responseCode: false
+  };
+
+  let outcomeSent = false;
+  const parsedUrl = url.parse(check.protocol + '://' + check.url, true);
+  const hostName = parsedUrl.hostname;
+  // Using path not pathname because we want the full query string
+  const path = parsedUrl.path;
+
+  // Build request
+  const requestDetails = {
+    protocol: check.protocol + ':',
+    hostName,
+    method: check.method.toUpperCase(),
+    path,
+    timeout: check.timeoutSeconds * 1000
+  };
+
+  // Send out request via the method the user requested in their health check
+  // Use either http OR https module
+  const _moduleToUse = check.protocol === 'http' ? http : https;
+  const req = _moduleToUse.request(requestDetails, res => {
+    // Get status code
+    const status = res.statusCode;
+    // Update outcome object
+    outcome.status = status;
+    if (!outcomeSent) {
+      workers.processCheckOutcome(check, outcome);
+      outcomeSent = true;
+    }
+  });
+
+  // Bind to err so it doesnt get thrown
+  req.on('error', err => {
+    // Update outcome and pass it along
+    outcome.error = { error: true, value: err };
+    if (!outcomeSent) {
+      workers.processCheckOutcome(check, outcome);
+      outcomeSent = true;
+    }
+  });
+
+  // Bind to timeout
+  req.on('timeout', err => {
+    // Update outcome and pass it along
+    outcome.error = { error: true, value: 'timeout' };
+    if (!outcomeSent) {
+      workers.processCheckOutcome(check, outcome);
+      outcomeSent = true;
+    }
+  });
+
+  // End/send it!
+  req.end();
+};
+
+/**
+ * Process the check outcome, update check data as needed, trigger an alert
+ * if needed. We need special logic for a check that instantiated in a down state,
+ * but is brought up for the first time
+ *
+ * @param {check} the check
+ * @param {outcome} the outcome object
+ */
+workers.processCheckOutcome = (check, outcome) => {
+  // Is the check in an up or down state?
+  const state =
+    !outcome.error &&
+    outcome.responseCode &&
+    check.successCodes.indexOf(outcome.responseCode) > -1
+      ? 'up'
+      : 'down';
+
+  // Does the current state warrant an SMS alert to the user?
+  // If the lastChecked field is set, we know the check has been checked before
+  // and is not newly instantiated - if so, if the state has changed, alert
+  // the user.
+  const alertWarranted = check.lastChecked && check.state !== state;
+
+  // Set new check
+  const newCheck = { ...check, state, lastChecked: Date.now() };
+
+  // Update the checks collection
+  _lib.update(CHECKS_DIR, newCheck.id, newCheck, err => {
+    if (!err) {
+      // Alert user?
+      if (alertWarranted) {
+        // Alert user
+        workers.alertUserToStateChange(newCheck);
+      } else {
+        // Check state not changed, do nothing
+        console.log('Check ' + newCheck.id + ' state unchanged');
+      }
+    } else {
+      console.log('Error updating check ' + newCheck.id);
+    }
+  });
+};
 
 /**
  * Sanity checking the check data
